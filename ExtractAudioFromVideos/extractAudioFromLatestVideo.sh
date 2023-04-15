5991864e282e85eaad4592e850d6d818aa0b796b
Dave Nice <dave.nice@uk.ibm.com>
2023-04-15 14:40:26 +0100

    finish script and update paths for mac

--- ExtractAudioFromVideos/extractAudioFromLatestVideo.sh
+++ ExtractAudioFromVideos/extractAudioFromLatestVideo.sh
@@ -1,40 +1,88 @@
 #!/bin/bash
 
-VIDEO_LOCATION=/Users/niced/tmp/videofolder
-BACKUP_LOCATION=/Users/niced/tmp/backupfolder
-ONEDRIVE_VIDEO_LOCATION=/Users/niced/tmp/onedrivefolder
-ONEDRIVE_AUDIO_LOCATION=/Users/niced/tmp/onedrivefolder
+VIDEO_LOCATION="/Users/avteam/Movies"
+BACKUP_LOCATION="/Volumes/Seagate Backup Plus Drive/ABC Service Videos Backup"
+ONEDRIVE_VIDEO_LOCATION="/Users/avteam/OneDrive - Above Bar Church/Recordings - Sundays/Raw Video"
+ONEDRIVE_AUDIO_LOCATION="/Users/avteam/OneDrive - Above Bar Church/Recordings - Sundays/Raw Audio"
+FFMPEG=/Users/avteam/bin
 
 MARKER_FILE=DoNotDelete-ProcessedToHere.mp4
 
+# check source and target locations exist, exit with error if not
+if [ ! -e $VIDEO_LOCATION ]
+then
+	echo "Cannot access source videos at $VIDEO_LOCATION"
+	exit 10
+fi
+if [ ! -e $BACKUP_LOCATION ]
+then
+	echo "Cannot backup - backup location $BACKUP_LOCATION doesn't exist."
+	exit 11
+fi
+if [ ! -e $ONEDRIVE_VIDEO_LOCATION ]
+then
+	echo "Cannot access onedrive folder - $ONEDRIVE_VIDEO_LOCATION"
+	exit 12
+fi
+
+# create a marker file if it doesn't exist, so the find command in the for loop doesn't fail
 if [ ! -e $VIDEO_LOCATION/$MARKER_FILE ]
 then
-	# todo - remove echo
-	echo	touch $VIDEO_LOCATION/$MARKER_FILE
+	echo "Updating marker file early - $VIDEO_LOCATION/$MARKER_FILE"
+	touch $VIDEO_LOCATION/$MARKER_FILE
 fi
 
 cd $VIDEO_LOCATION
 for INFILEMP4 in `find . -newer $MARKER_FILE -name "*.mp4"`
 do
-	HASH=`md5sum $INFILEMP4`
-	echo $HASH
-# outfile mp3
+	cd $VIDEO_LOCATION
+	echo "Processing file $INFILEMP4"
+	MP4HASH=`md5sum $INFILEMP4`
+	echo $MP4HASH
+	# outfile mp3
 	OUTFILEMP3=`echo $INFILEMP4 | sed 's/mp4/mp3/'`
-	echo $OUTFILEMP3
-# todo - remove echo
-echo ffmpeg -i $INFILEMP4 -codec:a libmp3lame -q:a 3 $OUTFILEMP3
-# check for mp3
+	echo "Generating MP3 to $OUTFILEMP3"
+	$FFMPEG -i $INFILEMP4 -codec:a libmp3lame -q:a 3 $OUTFILEMP3
+	# check for mp3
+	if [ ! -e $OUTFILEMP3 ]
+	then
+		echo "MP3 file was not created at $OUTFILEMP3"
+	fi
 
-# back up to external drive, check hash
-echo cp $INFILEMP4 $BACKUP_LOCATION
+	# back up to external drive, check hash
+	echo "Backing up $INFILEMP4 to backup location $BACKUP_LOCATION"
+	cp $INFILEMP4 $BACKUP_LOCATION
+	cd $BACKUP_LOCATION
+	MP4HASH_BACKUP=`md5sum $INFILEMP4`
+	if test "$MP4HASH" != "$MP4HASH_BACKUP";
+	then
+		echo "MP4 backup file does not have the same hash as the original"
+		exit 13
+	fi
 
-# move to onedrive
-echo mv $INFILEMP4 $ONEDRIVE_VIDEO_LOCATION
-echo mv $INFILEMP3 $ONEDRIVE_AUDIO_LOCATION
+	# move to onedrive
+	echo "Moving files to OneDrive"
+	if [ -e $VIDEO_LOCATION/$INFILEMP4 ]
+	then
+		echo "Moving $VIDEO_LOCATION/$INFILEMP4 to $ONEDRIVE_VIDEO_LOCATION"
+		mv $VIDEO_LOCATION/$INFILEMP4 $ONEDRIVE_VIDEO_LOCATION
+	fi
+	if [ -e $VIDEO_LOCATION/$OUTFILEMP3 ]
+	then
+		echo "Moving $VIDEO_LOCATION/$OUTFILEMP3 to $ONEDRIVE_AUDIO_LOCATION"
+		mv $VIDEO_LOCATION/$OUTFILEMP3 $ONEDRIVE_AUDIO_LOCATION
+	fi
 
-#check hash
+	#check hash
+	cd $ONEDRIVE_VIDEO_LOCATION
+	MP4HASH_ONEDRIVE=`md5sum $INFILEMP4`
+	if test "$MP4HASH" != "$MP4HASH_ONEDRIVE";
+	then
+		echo "MP4 file moved to onedrive does not have the same hash as the original"
+		exit 14
+	fi
 
 done
 
-# todo - remove echo
-echo	touch $VIDEO_LOCATION/$MARKER_FILE
+echo "Updating marker file late - $MARKER_FILE"
+touch $VIDEO_LOCATION/$MARKER_FILE
